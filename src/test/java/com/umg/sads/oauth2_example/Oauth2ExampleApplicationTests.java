6305e4207310cdb700f6c2fa756b8bd4cc13f931
rhre <robinson-roca@hotmail.com>
2024-09-27 11:18:44 -0600

    First commit

---
+++ src/test/java/com/umg/sads/oauth2_example/Oauth2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.sads.oauth2_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
